5e341910bef6af4ec77693a18b49917a6e7e7008
kawang <kawang@microstrategy.com>
2024-06-26 01:13:45 +0800

    add

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,13 @@ import routes from "./routes";
 import bodyParser from "body-parser";
 import "dotenv/config";
 import { App, createNodeMiddleware } from "@octokit/app";
+// import {  } from "octokit/webhooks";
+import fs from "fs";
+import { Octokit } from "octokit";
 
 // App
 const app = express();
+const privateKey = fs.readFileSync(process.env.PRIVATE_KEY_PATH, "utf8");
 
 app.use(
   bodyParser.json({
@@ -15,10 +19,10 @@ app.use(
   })
 );
 
-
+const enterpriseHostname = process.env.ENTERPRISE_HOSTNAME
 const octokitApp = new App({
   appId: process.env.APP_ID,
-  privateKey: process.env.PRIVATE_KEY,
+  privateKey: privateKey,
   webhooks: {
     secret: process.env.SECRET,
   },
@@ -26,11 +30,19 @@ const octokitApp = new App({
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
   },
+  ...(enterpriseHostname && {
+    Octokit: Octokit.defaults({
+      baseUrl: `https://${enterpriseHostname}/api/v3`
+    })
+  })
 });
+
+const { data } = await octokitApp.octokit.request('/app')
+const messageForNewPRs = "Thanks for opening a new PR! Please follow our contributing guidelines to make your PR easier to review.";
 
 async function handlePullRequestOpened({octokit, payload}) {
   console.log(`Received a pull request event for #${payload.pull_request.number}`);
-  const messageForNewPRs = "Thanks for opening a new PR! Please follow our contributing guidelines to make your PR easier to review.";
+  
 
   try {
     await octokit.request("POST /repos/{owner}/{repo}/issues/{issue_number}/comments", {
@@ -51,6 +63,7 @@ async function handlePullRequestOpened({octokit, payload}) {
 };
 
 octokitApp.webhooks.on("pull_request.opened", handlePullRequestOpened)
+octokitApp.webhooks.on("pull_request.reopened", handlePullRequestOpened)
 octokitApp.webhooks.onAny((event) => {
   console.log(event)
 })
@@ -62,6 +75,7 @@ app.use(
 // Set port
 const port: string | number = process.env.PORT || "1337";
 app.set("port", port);
+app.set("path", process.env.PATH || "/webhooks");
 
 // app.use("/", routes);
 
